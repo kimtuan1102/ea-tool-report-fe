f650b68b947199f976aaa87096011fd2af7a8ebf
kimtuan1102 <kimtuan1102@gmail.com>
2021-03-01 14:47:21 +0700

    change env

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Vue from 'vue'
 const $http = axios.create({
-  baseURL: process.env.VUE_APP_BASE_API,
+  baseURL: process.env.VUE_APP_BASE_API_PRIVATE,
 })
 Vue.prototype.$http = $http
 export default $http
